e55f132afc20ffd3a3568a5774c828e3f22c97ea
AleksanderGondek <gondekaleksander@hotmail.com>
2017-04-29 14:13:24 +0200

    Improvements to datastore models
    * Fixed issue with invalid relationship of Requirement class
    * Changed __repr__ behaviour
    * Added __str__ method

--- api/pipwatch_api/datastore/models.py
+++ api/pipwatch_api/datastore/models.py
@@ -11,9 +11,17 @@ class Tag(DATABASE.Model):
     """Represents a single tag, which can be assigned to mark a project."""
     name = DATABASE.Column(DATABASE.String(length=60, convert_unicode=True), primary_key=True)
 
-    def __repr__(self):
-        """Return class representation."""
-        return "<Tag {0!r}".format(self.name)
+    def __init__(self, name: str = "") -> None:
+        """Represents a single project entry."""
+        self.name = name
+
+    def __str__(self) -> str:
+        """Return class instance human-friendly representation."""
+        return "<Tag {0!r}>".format(self.name)
+
+    def __repr__(self) -> str:
+        """Return class instance representation."""
+        return "{0}({1!r})".format(self.__class__.__module__ + "." + self.__class__.__name__, self.name)
 
 
 TAGS = DATABASE.Table("tags",
@@ -29,9 +37,17 @@ class Namespace(DATABASE.Model):
     name = DATABASE.Column(DATABASE.String(length=200, convert_unicode=True), primary_key=True)
     projects = DATABASE.relationship("Project", backref="namespace", lazy="dynamic")
 
-    def __repr__(self):
+    def __init__(self, name: str = "") -> None:
+        """Represents a single project entry."""
+        self.name = name
+
+    def __str__(self) -> str:
         """Return class representation."""
-        return "<Namespace {0!r}".format(self.name)
+        return "<Namespace {0!r}>".format(self.name)
+
+    def __repr__(self) -> str:
+        """Return class instance representation."""
+        return "{0}({1!r})".format(self.__class__.__module__ + "." + self.__class__.__name__, self.name)
 
 
 class Project(DATABASE.Model):
@@ -44,17 +60,24 @@ class Project(DATABASE.Model):
                                    DATABASE.ForeignKey("namespace.name"))
 
     tags = DATABASE.relationship("Tag", secondary=TAGS, backref=DATABASE.backref("projects", lazy="dynamic"))
-    requirements_files = DATABASE.relationship("requirementsfiles", backref="project", lazy="dynamic")
+    requirements_files = DATABASE.relationship("RequirementsFile", backref="project", lazy="dynamic")
 
-    def __init__(self, name: str = "", namespace: str = "", url: str = ""):
+    def __init__(self, name: str = "", namespace: str = "", url: str = "", namespace_id: str = "") -> None:
         """Initialize class instance."""
         self.name = name
         self.namespace = namespace
         self.url = url
 
-    def __repr__(self):
+        self.namespace_id = namespace_id
+
+    def __str__(self) -> str:
         """Return class representation."""
-        return "<Project {0!r} {1!r}>".format(self.namespace, self.name)
+        return "<Project {1!r} from {0!r}>".format(self.namespace, self.name)
+
+    def __repr__(self) -> str:
+        """Return class instance representation."""
+        return "{0}({1!r},{2!r},{3!r})".format(self.__class__.__module__ + "." + self.__class__.__name__,
+                                               self.id, self.name, self.namespace_id)
 
 
 class RequirementsFile(DATABASE.Model):
@@ -65,11 +88,23 @@ class RequirementsFile(DATABASE.Model):
     status = DATABASE.Column(DATABASE.String(length=30, convert_unicode=True), unique=False, nullable=False)
 
     project_id = DATABASE.Column(DATABASE.Integer, DATABASE.ForeignKey("project.id"))
-    requirements = DATABASE.relationship("Requirement", backref="requirement0s_file", lazy="dynamic")
+    requirements = DATABASE.relationship("Requirement", backref="requirements_file", lazy="dynamic")
 
-    def __repr__(self):
+    def __init__(self, full_path: str = "", status: str = "", project_id: int = -1) -> None:
+        """Initialize class instance."""
+        self.full_path = full_path
+        self.status = status
+
+        self.project_id = project_id
+
+    def __str__(self) -> str:
         """Return class representation."""
         return "<RequirementsFile {0!r}>".format(self.full_path)
+
+    def __repr__(self) -> str:
+        """Return class instance representation."""
+        return "{0}({1!r},{2!r},{3!r},{4!r})".format(self.__class__.__module__ + "." + self.__class__.__name__,
+                                                     self.id, self.full_path, self.status, self.project_id)
 
 
 class Requirement(DATABASE.Model):
@@ -83,6 +118,27 @@ class Requirement(DATABASE.Model):
     status = DATABASE.Column(DATABASE.String(length=30, convert_unicode=True), unique=False, nullable=False)
     requirements_file_id = DATABASE.Column(DATABASE.Integer, DATABASE.ForeignKey("requirements_file.id"))
 
-    def __repr__(self):
+    def __init__(self, name: str = "", current_version: str = "",  # pylint: disable=too-many-arguments
+                 desired_version: str = "", status: str = "",
+                 requirements_file_id: int = -1) -> None:
+        """Initialize class instance."""
+        self.name = name
+        self.current_version = current_version
+        self.desired_version = desired_version
+        self.status = status
+
+        self.requirements_file_id = requirements_file_id
+
+    def __str__(self) -> str:
         """Return class representation."""
         return "<Requirement {0!r} ({1!r})>".format(self.name, self.current_version)
+
+    def __repr__(self) -> str:
+        """Return class instance representation."""
+        return "{0}({1!r},{2!r},{3!r},{4!r},{5!r},{6!r})".format(self.__class__.__module__ + "." +
+                                                                 self.__class__.__name__,
+                                                                 self.id, self.name,
+                                                                 self.current_version,
+                                                                 self.desired_version,
+                                                                 self.status,
+                                                                 self.requirements_file_id)
