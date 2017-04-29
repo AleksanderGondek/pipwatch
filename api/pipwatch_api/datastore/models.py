1fe6b6f81d0dfe3967d18e84eb9876b2466ceb88
AleksanderGondek <gondekaleksander@hotmail.com>
2017-04-29 15:41:51 +0200

    Added id column to all tables
    This will allow for unification of data-access code.
    Additionaly removed printing of id in repr methods.

--- api/pipwatch_api/datastore/models.py
+++ api/pipwatch_api/datastore/models.py
@@ -9,7 +9,8 @@ DATABASE = SQLAlchemy()
 
 class Tag(DATABASE.Model):
     """Represents a single tag, which can be assigned to mark a project."""
-    name = DATABASE.Column(DATABASE.String(length=60, convert_unicode=True), primary_key=True)
+    id = DATABASE.Column(DATABASE.Integer, primary_key=True)  # pylint: disable=invalid-name
+    name = DATABASE.Column(DATABASE.String(length=60, convert_unicode=True), unique=True, nullable=False)
 
     def __init__(self, name: str = "") -> None:
         """Represents a single project entry."""
@@ -25,16 +26,14 @@ class Tag(DATABASE.Model):
 
 
 TAGS = DATABASE.Table("tags",
-                      DATABASE.Column("tag_name",
-                                      DATABASE.String(length=60, convert_unicode=True),
-                                      DATABASE.ForeignKey("tag.name")),
-                      DATABASE.Column("project_id", DATABASE.Integer,
-                                      DATABASE.ForeignKey("project.id")))
+                      DATABASE.Column("tag_id", DATABASE.Integer, DATABASE.ForeignKey("tag.id")),
+                      DATABASE.Column("project_id", DATABASE.Integer, DATABASE.ForeignKey("project.id")))
 
 
 class Namespace(DATABASE.Model):
     """Represents a single namespace, which can contain multiple projects."""
-    name = DATABASE.Column(DATABASE.String(length=200, convert_unicode=True), primary_key=True)
+    id = DATABASE.Column(DATABASE.Integer, primary_key=True)  # pylint: disable=invalid-name
+    name = DATABASE.Column(DATABASE.String(length=200, convert_unicode=True), unique=True, nullable=False)
     projects = DATABASE.relationship("Project", backref="namespace", lazy="dynamic")
 
     def __init__(self, name: str = "") -> None:
@@ -56,13 +55,12 @@ class Project(DATABASE.Model):
     name = DATABASE.Column(DATABASE.String(length=200, convert_unicode=True), unique=False, nullable=False)
     url = DATABASE.Column(DATABASE.String(length=400, convert_unicode=True), unique=True, nullable=False)
 
-    namespace_id = DATABASE.Column(DATABASE.String(length=200, convert_unicode=True),
-                                   DATABASE.ForeignKey("namespace.name"))
+    namespace_id = DATABASE.Column(DATABASE.Integer, DATABASE.ForeignKey("namespace.id"))
 
     tags = DATABASE.relationship("Tag", secondary=TAGS, backref=DATABASE.backref("projects", lazy="dynamic"))
     requirements_files = DATABASE.relationship("RequirementsFile", backref="project", lazy="dynamic")
 
-    def __init__(self, name: str = "", namespace: str = "", url: str = "", namespace_id: str = "") -> None:
+    def __init__(self, name: str = "", namespace: str = "", url: str = "", namespace_id: int = "") -> None:
         """Initialize class instance."""
         self.name = name
         self.namespace = namespace
@@ -76,8 +74,8 @@ class Project(DATABASE.Model):
 
     def __repr__(self) -> str:
         """Return class instance representation."""
-        return "{0}({1!r},{2!r},{3!r})".format(self.__class__.__module__ + "." + self.__class__.__name__,
-                                               self.id, self.name, self.namespace_id)
+        return "{0}({1!r},{2!r})".format(self.__class__.__module__ + "." + self.__class__.__name__,
+                                         self.name, self.namespace_id)
 
 
 class RequirementsFile(DATABASE.Model):
@@ -103,8 +101,8 @@ class RequirementsFile(DATABASE.Model):
 
     def __repr__(self) -> str:
         """Return class instance representation."""
-        return "{0}({1!r},{2!r},{3!r},{4!r})".format(self.__class__.__module__ + "." + self.__class__.__name__,
-                                                     self.id, self.full_path, self.status, self.project_id)
+        return "{0}({1!r},{2!r},{3!r})".format(self.__class__.__module__ + "." + self.__class__.__name__,
+                                                     self.full_path, self.status, self.project_id)
 
 
 class Requirement(DATABASE.Model):
@@ -135,10 +133,10 @@ class Requirement(DATABASE.Model):
 
     def __repr__(self) -> str:
         """Return class instance representation."""
-        return "{0}({1!r},{2!r},{3!r},{4!r},{5!r},{6!r})".format(self.__class__.__module__ + "." +
-                                                                 self.__class__.__name__,
-                                                                 self.id, self.name,
-                                                                 self.current_version,
-                                                                 self.desired_version,
-                                                                 self.status,
-                                                                 self.requirements_file_id)
+        return "{0}({1!r},{2!r},{3!r},{4!r},{5!r})".format(self.__class__.__module__ + "." +
+                                                           self.__class__.__name__,
+                                                           self.name,
+                                                           self.current_version,
+                                                           self.desired_version,
+                                                           self.status,
+                                                           self.requirements_file_id)
