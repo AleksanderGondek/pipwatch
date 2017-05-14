47265d505c988ac10512490073dbfe851bf30383
AleksanderGondek <gondekaleksander@hotmail.com>
2017-05-14 21:08:53 +0200

    Initial draft on DefaultStore tests fixes

--- api/tests/datastore/test_stores.py
+++ api/tests/datastore/test_stores.py
@@ -1,24 +1,35 @@
 import pytest
 
-from pipwatch_api.datastore.models import Tag
 from pipwatch_api.datastore.stores import DefaultStore
+
+
+class ModelMock():
+    id = 0
+    name = ""
+    __table__ = object()
+
+    def __init__(self, name: str = "") -> None:
+        self.name = name
 
 
 @pytest.fixture()
 def default_store_fixture(database) -> DefaultStore:
     """To be described."""
-    return DefaultStore(model=Tag, database=database)
+    return DefaultStore(model=ModelMock, database=database)
 
 
 class TestDefaultStore():
-    """To be described."""
-    def test_naive_get_columns_names(self, default_store_fixture) -> None:
-        """To be described."""
+    """Test behaviour of DefaultStore class."""
+    def test_naive_get_columns_names(self, default_store_fixture, mocker) -> None:
+        """Should return all column names for given entity, except for blacklisted ones."""
+        mocker.patch.object(ModelMock, "__table__")
+        ModelMock.__table__.columns.keys.return_value = ["id", "name"]
         assert default_store_fixture._naive_get_columns_names() == ["name"]
 
     def test_create(self, default_store_fixture, mocker) -> None:
-        """To be described."""
+        """Should create a valid instance of entity, based on passed in dictionary."""
         mocker.spy(default_store_fixture, "_additional_document_handler")
+        mocker.patch.object(default_store_fixture.database.session, "add", autospec=True)
         mocker.patch.object(default_store_fixture, "_naive_get_columns_names",
                             autospec=True, return_value=["name"])
         dictionary_representation_of_database_entity = {
@@ -30,4 +41,5 @@ class TestDefaultStore():
 
         assert getattr(entity_returned, "name", None) == "steffan muss"
         assert getattr(entity_returned, "other_property", None) == None
+        default_store_fixture.database.session.add.assert_called_once()
         default_store_fixture._additional_document_handler.assert_called_once()
