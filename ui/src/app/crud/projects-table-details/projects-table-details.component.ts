7cddf6f364369349c557d3f05af1bd91e39cb36c
AleksanderGondek <gondekaleksander@hotmail.com>
2017-06-13 19:21:58 +0200

    CRUD - Projects Details - Edit mockup expanded

--- ui/src/app/crud/projects-table-details/projects-table-details.component.ts
+++ ui/src/app/crud/projects-table-details/projects-table-details.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from "@angular/core";
 
+import { Project } from "../../data-brokers/entities";
+
 @Component({
   selector: "app-projects-table-details",
   templateUrl: "./projects-table-details.component.html",
   styleUrls: ["./projects-table-details.component.css"]
 })
 export class ProjectsTableDetailsComponent implements OnInit {
+    public project: Project;
 
-  constructor() { }
+    constructor() {
+        this.project = new Project({});
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
