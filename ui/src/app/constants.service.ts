e6c8e955d07c2e76e6ce156ce241694d7f7e3051
AleksanderGondek <gondekaleksander@hotmail.com>
2017-06-19 18:57:39 +0200

    UI - Constants Service - Pilot usage added

--- ui/src/app/constants.service.ts
+++ ui/src/app/constants.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class ConstantsService {
-    public API_URL = "http://127.0.0.1:8080/api/";
+    public static API_URL = "http://127.0.0.1:8080/api/";
 
     constructor() { }
 }
--- ui/src/app/crud/tags-table/tags-table.component.ts
+++ ui/src/app/crud/tags-table/tags-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 
+import { ConstantsService } from "../../constants.service";
 import { DataTableHandler } from "../../data-brokers/data-table-handler";
 import { EntityTable } from "../entity-table/entity-table";
 import { Tag } from "../../data-brokers/entities";
@@ -10,11 +11,11 @@ import { Tag } from "../../data-brokers/entities";
   styleUrls: ["./tags-table.component.css"]
 })
 export class TagsTableComponent extends EntityTable<Tag> implements OnInit {
-  constructor(broker: DataTableHandler<Tag>) {
+  constructor(broker: DataTableHandler<Tag>, constants: ConstantsService) {
       super(broker);
   }
 
   ngOnInit() {
-      this.initialize("http://127.0.0.1:8080/api/v1/tags/", Tag);
+      this.initialize(ConstantsService.API_URL + "v1/tags/", Tag);
   }
 }
