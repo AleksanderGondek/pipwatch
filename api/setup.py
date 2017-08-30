2e1dbc9dd191c437059e1a85eb2fd717dd1e63c0
AleksanderGondek <gondekaleksander@hotmail.com>
2017-08-30 22:10:06 +0200

    Added building of setup.py package to CI process

--- api/setup.py
+++ api/setup.py
@@ -5,5 +5,10 @@ from pipwatch_api.version import VERSION
 setup(
     name="pipwatch_api",
     version=VERSION,
-    packages=find_packages(),
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        "celery[redis]"
+    ],
+    include_package_data=False,
+    license="Apache-2.0",
 )
--- worker/setup.py
+++ worker/setup.py
@@ -5,5 +5,11 @@ from pipwatch_worker.version import VERSION
 setup(
     name="pipwatch_worker",
     version=VERSION,
-    packages=find_packages(),
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        "flask-restplus",
+        "Flask-SQLAlchemy"
+    ],
+    include_package_data=False,
+    license="Apache-2.0",
 )
