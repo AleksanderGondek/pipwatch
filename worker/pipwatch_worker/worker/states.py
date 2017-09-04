8f421dfc9580424b2de42c177c9ef9f41cd68351
AleksanderGondek <gondekaleksander@hotmail.com>
2017-09-04 20:37:50 +0200

    Worker - Initial skeleton of worker class added

--- worker/pipwatch_worker/worker/states.py
+++ worker/pipwatch_worker/worker/states.py
@@ -3,10 +3,9 @@
 from enum import Enum
 
 
-class WorkerStates(Enum):
+class States(Enum):
     ATTEMPTING_UPDATE: str = "Attempting_Update"
     CHECKING_FOR_UPDATES: str = "Checking_For_Updates"
-    CLEANING_UP: str = "Cleaning_Up"
     CLONING_REPOSITORY: str = "Cloning_Repository"
     COMMITTING_CHANGES: str = "Committing_Changes"
     FAILURE: str = "Failure"
@@ -16,14 +15,61 @@ class WorkerStates(Enum):
     UPDATING_METADATA: str = "Updating_Metadata"
 
 
-# pylint: disable=line-too-long
+class Triggers(Enum):
+    TO_CHECK_UPDATES: str = "to_check_updates"
+    TO_CLONE: str = "to_clone"
+    TO_COMMIT: str = "to_commit_changes"
+    TO_FAIL: str = "to_fail"
+    TO_PARSE_REQ: str = "to_parse_requirements"
+    TO_SUCCESS: str = "to_success"
+    TO_UPDATE_META: str = "to_update_metadata"
+    TO_UPDATE_PGS: str = "to_attempt_update"
+
+
 WORKER_STATE_TRANSITIONS = [
-    {"trigger": "fail", "source": "*", "dest": WorkerStates.FAILURE.value},
-    {"trigger": "clone", "source": WorkerStates.INITIALIZING.value, "dest": WorkerStates.CLONING_REPOSITORY.value},  # noqa: E501
-    {"trigger": "parse_requirements", "source": WorkerStates.CLONING_REPOSITORY.value, "dest": WorkerStates.PARSING_REQUIREMENTS.value},  # noqa: E501
-    {"trigger": "update_metadata", "source": WorkerStates.PARSING_REQUIREMENTS.value, "dest": WorkerStates.UPDATING_METADATA.value},  # noqa: E501
-    {"trigger": "attempt_update", "source": WorkerStates.UPDATING_METADATA.value, "dest": WorkerStates.ATTEMPTING_UPDATE.value},  # noqa: E501
-    {"trigger": "commit_changes", "source": WorkerStates.ATTEMPTING_UPDATE.value, "dest": WorkerStates.COMMITTING_CHANGES.value},  # noqa: E501
-    {"trigger": "update_metadata", "source": WorkerStates.COMMITTING_CHANGES.value, "dest": WorkerStates.UPDATING_METADATA.value},  # noqa: E501
-    {"trigger": "success", "source": WorkerStates.UPDATING_METADATA.value, "dest": WorkerStates.SUCCESS.value}  # noqa: E501
+    {
+        "source": "*",
+        "dest": States.FAILURE.value,
+        "trigger": Triggers.TO_FAIL.value
+    },
+    {
+        "source": States.INITIALIZING.value,
+        "dest": States.CLONING_REPOSITORY.value,
+        "trigger": Triggers.TO_CLONE.value
+    },
+    {
+        "source": States.CLONING_REPOSITORY.value,
+        "dest": States.PARSING_REQUIREMENTS.value,
+        "trigger": Triggers.TO_PARSE_REQ.value
+    },
+    {
+        "source": States.PARSING_REQUIREMENTS.value,
+        "dest": States.CHECKING_FOR_UPDATES.value,
+        "trigger": Triggers.TO_CHECK_UPDATES.value
+    },
+    {
+        "source": States.CHECKING_FOR_UPDATES.value,
+        "dest": States.UPDATING_METADATA.value,
+        "trigger": Triggers.TO_UPDATE_META.value
+    },
+    {
+        "source": States.UPDATING_METADATA.value,
+        "dest": States.ATTEMPTING_UPDATE.value,
+        "trigger": Triggers.TO_UPDATE_PGS.value
+    },
+    {
+        "source": States.ATTEMPTING_UPDATE.value,
+        "dest": States.COMMITTING_CHANGES.value,
+        "trigger": Triggers.TO_COMMIT.value
+    },
+    {
+        "source": States.COMMITTING_CHANGES.value,
+        "dest": States.UPDATING_METADATA.value,
+        "trigger": Triggers.TO_UPDATE_META.value
+    },
+    {
+        "source": States.UPDATING_METADATA.value,
+        "dest": States.SUCCESS.value,
+        "trigger": Triggers.TO_SUCCESS.value
+    }
 ]
--- worker/pipwatch_worker/worker/worker.py
+++ worker/pipwatch_worker/worker/worker.py
@@ -1,21 +1,91 @@
 """This module contains pipwatch worker definition."""
 
 from logging import getLogger, Logger  # noqa: F401 Imported for type definition
+from typing import Callable
 
 from transitions import Machine
 
-from pipwatch_worker.worker.states import WorkerStates, WORKER_STATE_TRANSITIONS
+from pipwatch_worker.worker.states import States, WORKER_STATE_TRANSITIONS, Triggers
 
 
 class Worker:
     """Responsible for checking and updating python packages in given project."""
 
-    def __init__(self, logger: Logger = None) -> None:
+    def __init__(self, update_celery_state_method: Callable[[str], None],
+                 logger: Logger = None) -> None:
         """Initialize worker instance."""
         self.log: Logger = logger or getLogger(__name__)
         self.state_machine = Machine(
             model=self,
-            states=[state.value for state in WorkerStates],
-            initial=WorkerStates.INITIALIZING.value,
+            states=[state.value for state in States],
+            initial=States.INITIALIZING.value,
             transitions=WORKER_STATE_TRANSITIONS
         )
+
+        self.update_celery_state = update_celery_state_method
+        self.should_attempt_update = False
+
+    def run(self) -> None:
+        """Start worker processing of project requirements update request."""
+        try:
+            self.initialize()
+            self.clone()
+            self.parse_requirements()
+            self.check_updates()
+            self.update_metadata()
+
+            if self.should_attempt_update:
+                self.attempt_update()
+                self.commit_changes()
+
+            self.success()
+        except Exception:
+            self.log.exception("Was unable to process update request for project.")
+            self.fail()
+
+    def fail(self) -> None:
+        """Signify that processing of given request has failed."""
+        self.trigger(Triggers.TO_FAIL.value)
+        self.update_celery_state(States.FAILURE.value)
+
+    def initialize(self) -> None:
+        """Initialize variables needed for further request processing."""
+        self.update_celery_state(States.INITIALIZING.value)
+
+    def clone(self) -> None:
+        """Clone repository containing given project."""
+        self.trigger(Triggers.TO_CLONE.value)
+        self.update_celery_state(States.CLONING_REPOSITORY.value)
+
+    def parse_requirements(self) -> None:
+        """Parse and load requirements that are needed by given project."""
+        self.trigger(Triggers.TO_PARSE_REQ.value)
+        self.update_celery_state(States.PARSING_REQUIREMENTS.value)
+
+    def check_updates(self) -> None:
+        """Check if any of given required packages may be updated."""
+        self.trigger(Triggers.TO_CHECK_UPDATES.value)
+        self.update_celery_state(States.CHECKING_FOR_UPDATES.value)
+
+    def update_metadata(self) -> None:
+        """Send update of given project information (with possible new requirements versions)."""
+        self.trigger(Triggers.TO_UPDATE_META.value)
+        self.update_celery_state(States.UPDATING_METADATA.value)
+
+    def attempt_update(self) -> None:
+        """Check if update of given packages will break project."""
+        self.trigger(Triggers.TO_UPDATE_PGS.value)
+        self.update_celery_state(States.ATTEMPTING_UPDATE.value)
+
+    def commit_changes(self) -> None:
+        """Commit changes to given project."""
+        self.trigger(Triggers.TO_COMMIT.value)
+        self.update_celery_state(States.COMMITTING_CHANGES.value)
+
+    def success(self) -> None:
+        """Signify that processing of given request has succeeded."""
+        self.trigger(Triggers.TO_SUCCESS.value)
+        self.update_celery_state(States.SUCCESS.value)
+
+    def trigger(self, transition_trigger: str) -> None:
+        """This will be overridden by transitions.Machine"""
