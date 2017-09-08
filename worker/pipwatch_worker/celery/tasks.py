75683891799f32999b0c4e41ed330b07cc6df987
AleksanderGondek <gondekaleksander@hotmail.com>
2017-09-08 18:14:00 +0200

    Worker - Initial sketch of how processing task may work

--- worker/pipwatch_worker/celery/tasks.py
+++ worker/pipwatch_worker/celery/tasks.py
@@ -1,9 +1,17 @@
 """This module contains celery tasks available for the worker."""
+from typing import Any, Dict
 
 from pipwatch_worker.celery.application import app
+from pipwatch_worker.core.data_models import Project
+from pipwatch_worker.worker.worker import Worker
 
 
-@app.task
-def task_example(number_one: int, number_two: int) -> int:
-    """Example of task, which adds two numbers."""
-    return number_one + number_two
+@app.task(bind=True, retry_backoff=True)
+def process_project(self, processing_request: Dict[str, Any]) -> None:
+    """Check if packages in given project may be updated."""
+    try:
+        worker = Worker(update_celery_state_method=self.update_state)
+        project_processing_request: Project = Project.from_dict(dictionary=processing_request)
+        worker.run(project_to_process=project_processing_request)
+    except Exception as exc:
+        raise self.retry(exc=exc)
