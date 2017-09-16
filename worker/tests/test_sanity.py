34a50476d4277f4b1567b115efde0c405cae1896
AleksanderGondek <gondekaleksander@hotmail.com>
2017-09-16 22:58:23 +0200

    Worker - Expanded sanity test to check for celery app instance.

--- worker/tests/test_sanity.py
+++ worker/tests/test_sanity.py
@@ -2,3 +2,6 @@ def test_sanity() -> None:
     """Check if pytest is working."""
     from pipwatch_worker.main import main
     assert main is not None
+
+    from pipwatch_worker.celery.application import app
+    assert app is not None
