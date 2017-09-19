e3981295d50329cbfb4955fc284ee357d5a58c42
AleksanderGondek <gondekaleksander@hotmail.com>
2017-09-19 20:31:09 +0200

    Worker - Refactoring of attempting updates

--- worker/pipwatch_worker/worker/attempting_updates.py
+++ worker/pipwatch_worker/worker/attempting_updates.py
@@ -2,24 +2,21 @@
 import os
 
 from pipwatch_worker.core.data_models import Project, RequirementsFile
-from pipwatch_worker.worker.commands import Git
+from pipwatch_worker.worker.commands import FromVirtualenv, Git
 
 
 class AttemptUpdate:  # pylint: disable=too-few-public-methods
-    """Encapsulates logic of updating requirements of given project."""
-
-    DEFAULT_COMMIT_MESSAGE = "[Pipwatch] - Auto-update packages"
+    """Encapsulates logic of attempt of updating requirements of given project."""
 
     def __init__(
             self,
             repository_directory: str,
-            project_details: Project,
-            commit_message: str = None
+            project_details: Project
     ) -> None:
         """Create method instance."""
         self.repository_directory_name = repository_directory
         self.project_details = project_details
-        self.commit_message = self.DEFAULT_COMMIT_MESSAGE if not commit_message else commit_message
+        self.from_venv = FromVirtualenv(project_id=self.project_details.id)
         self.git = Git(
             project_id=self.project_details.id,
             project_url=self.project_details.url,
@@ -30,9 +27,13 @@ class AttemptUpdate:  # pylint: disable=too-few-public-methods
         """Update requirements of given project."""
         for requirements_file in self.project_details.requirements_files:
             self._update_requirement_file(requirements_file=requirements_file)
-            self.git("add {}".format(requirements_file.path))
+            self.from_venv(command="pip install -U -r {}".format(requirements_file.path))
 
-        self.git("commit -m '{commit_msg}'".format(commit_msg=self.commit_message))
+        self._check()
+
+    def _check(self) -> bool:  # pylint: disable=no-self-use
+        """Validate if new packages did not break the project."""
+        return True
 
     def _update_requirement_file(self, requirements_file: RequirementsFile) -> None:
         """Save new requirements."""
