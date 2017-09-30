c716dea2a8881dbef6c49d53fe83ac2dacefbab2
AleksanderGondek <gondekaleksander@hotmail.com>
2017-09-30 16:38:46 +0200

    Api - Added resource for checking update task status

--- api/pipwatch_api/namespaces/v1/projects_updates.py
+++ api/pipwatch_api/namespaces/v1/projects_updates.py
@@ -22,3 +22,16 @@ class ProjectsUpdate(Resource):
     def post(self, project_id: int):
         """Request update of requirements of project specified."""
         return self.updates_broker.send_update_request(project_id=project_id), 200
+
+
+@projects_updates_namespace.route("/<string:task_id>")
+class ProjectsUpdateStatus(Resource):
+    """Resource representing requirements update task status."""
+    def __init__(self, *args, **kwargs):
+        """Initialize resource instance."""
+        super().__init__(*args, **kwargs)
+        self.updates_broker = ProjectUpdateBroker()
+
+    def get(self, task_id: str):
+        """Return status of given update task."""
+        return self.updates_broker.check_task(task_id=task_id), 200
