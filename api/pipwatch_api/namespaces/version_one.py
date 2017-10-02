6600dcd7175cccefea43444c4ed5bdb0172ed34b
AleksanderGondek <gondekaleksander@hotmail.com>
2017-10-02 12:14:35 +0200

    Api - Added projects_updates namespace to api

--- api/pipwatch_api/namespaces/version_one.py
+++ api/pipwatch_api/namespaces/version_one.py
@@ -4,6 +4,7 @@ from flask_restplus import Api
 
 from pipwatch_api.namespaces.v1.namespaces import namespaces_namespace
 from pipwatch_api.namespaces.v1.projects import projects_namespace
+from pipwatch_api.namespaces.v1.projects_updates import projects_updates_namespace
 from pipwatch_api.namespaces.v1.requirements import requirements_namespace
 from pipwatch_api.namespaces.v1.requirements_files import requirements_files_namespace
 from pipwatch_api.namespaces.v1.status import status_namespace
@@ -22,6 +23,7 @@ def get_api_version_one() -> Blueprint:
     api_version_one.add_namespace(status_namespace)
     api_version_one.add_namespace(namespaces_namespace)
     api_version_one.add_namespace(projects_namespace)
+    api_version_one.add_namespace(projects_updates_namespace)
     api_version_one.add_namespace(requirements_namespace)
     api_version_one.add_namespace(requirements_files_namespace)
     api_version_one.add_namespace(tags_namespace)
--- api/tests/namespaces/test_version_one.py
+++ api/tests/namespaces/test_version_one.py
@@ -10,17 +10,19 @@ def test_get_api_version_one(mocker) -> None:
     status_ns_mock = mocker.patch("pipwatch_api.namespaces.version_one.status_namespace", autospec=True)
     namespaces_ns_mock = mocker.patch("pipwatch_api.namespaces.version_one.namespaces_namespace", autospec=True)
     projects_ns_mock = mocker.patch("pipwatch_api.namespaces.version_one.projects_namespace", autospec=True)
+    projects_updates_ns_mock = mocker.patch("pipwatch_api.namespaces.version_one.projects_updates_namespace", autospec=True)
     requirements_ns_mock = mocker.patch("pipwatch_api.namespaces.version_one.requirements_namespace", autospec=True)
     requirements_files_ns_mock = mocker.patch("pipwatch_api.namespaces.version_one.requirements_files_namespace", autospec=True)
     tags_ns_mock = mocker.patch("pipwatch_api.namespaces.version_one.tags_namespace", autospec=True)
 
     get_api_version_one()
 
-    assert api_mock.return_value.add_namespace.call_count == 6
+    assert api_mock.return_value.add_namespace.call_count == 7
     assert api_mock.return_value.add_namespace.mock_calls == [
         mocker.call(status_ns_mock),
         mocker.call(namespaces_ns_mock),
         mocker.call(projects_ns_mock),
+        mocker.call(projects_updates_ns_mock),
         mocker.call(requirements_ns_mock),
         mocker.call(requirements_files_ns_mock),
         mocker.call(tags_ns_mock)
