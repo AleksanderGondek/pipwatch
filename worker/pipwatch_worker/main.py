b8023ee05666f9873429c3de5c189746168629c2
AleksanderGondek <gondekaleksander@hotmail.com>
2017-10-04 15:48:25 +0200

    Worker - Refactored main.py to allow for sending celery tasks via cli

--- worker/pipwatch_worker/main.py
+++ worker/pipwatch_worker/main.py
@@ -1,21 +1,31 @@
-"""This module contains logic responsible for starting the pipwatch worker."""
+"""This module contains logic responsible for starting the pipwatch worker tasks via cli."""
 
-import sys
+import json
 from logging import getLogger, Logger  # noqa: F401 Imported for type definition
+import sys
 
 from pipwatch_worker.celery_components.application import app
 from pipwatch_worker.core.configuration import configure_logger
 
 
 def main() -> None:
-    """Function to start the whole application."""
+    """Function to start the given celery task."""
     configure_logger()
     log: Logger = getLogger(__name__)
 
-    log.info("Attempting to start worker")
-    app.start()
-    log.info("Worker started")
+    if len(sys.argv) <= 1:
+        log.info("Please provide name of task to run.")
+        sys.exit(1)
 
+    _, task_name, task_args, *_ = sys.argv  # type: ignore
+    log.info("Sending task %s.", task_name)
+    log.info("Task args (should be json): %s.", task_args)
+
+    result_async = app.send_task(task_name, args=json.loads(task_args), kwargs=None)
+    log.info("Waiting for task results.")
+    result_async.get()
+
+    log.info("Task finished.")
     sys.exit(0)
 
 
