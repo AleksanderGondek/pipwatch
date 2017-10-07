5b4beca672f1e42f22eaccca3aea0a4e6bd635a1
AleksanderGondek <gondekaleksander@hotmail.com>
2017-10-07 17:54:47 +0200

    Api - Added loading requirements to setup.py from txt file

--- api/setup.py
+++ api/setup.py
@@ -1,16 +1,25 @@
+import os
 from setuptools import setup, find_packages
+from typing import List
 
 from pipwatch_api.version import VERSION
+
+
+def read_requirements() -> List[str]:
+    """Read packages that are required by pipwatch api."""
+    current_working_directory = os.path.abspath(os.path.dirname(__file__))
+    requirements = []
+    with open(os.path.join(current_working_directory, "requirements.txt"), encoding="utf-8") as file:
+        requirements = [requirement.strip() for requirement in file.readlines()]
+
+    return requirements
+
 
 setup(
     name="pipwatch_api",
     version=VERSION,
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    install_requires=[
-        "celery[redis]",
-        "flask-restplus",
-        "Flask-SQLAlchemy"
-    ],
-    include_package_data=False,
+    install_requires=read_requirements(),
+    include_package_data=True,
     license="Apache-2.0",
 )
