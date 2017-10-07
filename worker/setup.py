0676e36c78e253d01911971e37d318d45e7ec048
AleksanderGondek <gondekaleksander@hotmail.com>
2017-10-07 17:59:48 +0200

    Worker - Added loading requirements to setup.py from txt file

--- worker/setup.py
+++ worker/setup.py
@@ -1,15 +1,25 @@
+import os
 from setuptools import setup, find_packages
+from typing import List
 
 from pipwatch_worker.version import VERSION
+
+
+def read_requirements() -> List[str]:
+    """Read packages that are required by pipwatch worker."""
+    current_working_directory = os.path.abspath(os.path.dirname(__file__))
+    requirements = []
+    with open(os.path.join(current_working_directory, "requirements.txt"), encoding="utf-8") as file:
+        requirements = [requirement.strip() for requirement in file.readlines()]
+
+    return requirements
+
 
 setup(
     name="pipwatch_worker",
     version=VERSION,
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    install_requires=[
-        "flask-restplus",
-        "Flask-SQLAlchemy"
-    ],
-    include_package_data=False,
+    install_requires=read_requirements(),
+    include_package_data=True,
     license="Apache-2.0",
 )
