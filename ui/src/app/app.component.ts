b646250e2547e9f4acab2cd9400891837196f233
AleksanderGondek <gondekaleksander@hotmail.com>
2017-11-01 14:57:20 +0100

    Ui - Change codestyle to double quotes

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 import { DataBroker } from "./data-brokers/data-broker";
 import { Project } from "./data-brokers/entities";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent  implements OnInit {
-  title = 'app works!';
+  title = "app works!";
   listOfProjects = new Array<Project>();
   constructor(private readonly broker: DataBroker<Project>) {
   }
