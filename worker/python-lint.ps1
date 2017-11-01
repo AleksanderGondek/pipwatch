b28c098c555001a77c02c1cb647781851fd69d79
AleksanderGondek <gondekaleksander@hotmail.com>
2017-11-01 14:25:15 +0100

    Worker - Rename pep8 into pycodestyle

--- worker/python-lint.ps1
+++ worker/python-lint.ps1
@@ -1,4 +1,4 @@
-﻿.\.virtualenv\Scripts\pep8.exe pipwatch_worker
+﻿.\.virtualenv\Scripts\pycodestyle.exe pipwatch_worker
 .\.virtualenv\Scripts\pylint.exe pipwatch_worker --rcfile=pylintrc
 .\.virtualenv\Scripts\flake8.exe pipwatch_worker
 .\.virtualenv\Scripts\mypy.exe --ignore-missing-imports pipwatch_worker
