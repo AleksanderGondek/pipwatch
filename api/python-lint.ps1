cef73536bc66f2ac2d1891db6b779eff70434d50
AleksanderGondek <gondekaleksander@hotmail.com>
2017-11-01 14:14:01 +0100

    Api - Rename pep8 into pycodestyle

--- api/python-lint.ps1
+++ api/python-lint.ps1
@@ -1,4 +1,4 @@
-﻿.\.virtualenv\Scripts\pep8.exe pipwatch_api
+﻿.\.virtualenv\Scripts\pycodestyle.exe pipwatch_api
 .\.virtualenv\Scripts\pylint.exe pipwatch_api --rcfile=pylintrc
 .\.virtualenv\Scripts\flake8.exe pipwatch_api
 .\.virtualenv\Scripts\mypy.exe --ignore-missing-imports pipwatch_api
