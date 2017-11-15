a03e92d728e3f5139d9cf8fdf2b11ec4ad5d7c0b
AleksanderGondek <gondekaleksander@hotmail.com>
2017-11-15 21:31:14 +0100

    Ui - Store created task id

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -11,22 +11,25 @@ import { ProjectsUpdateService } from "./data-brokers/projectsUpdateService";
   templateUrl: "./app.component.html",
 })
 export class AppComponent  implements OnInit {
-  title = "Hello, from pipwatch-ui!";
-  listOfProjects = new Array<Project>();
+    title = "Hello, from pipwatch-ui!";
+    triggeredTaskId = "";
+    listOfProjects = new Array<Project>();
 
-  constructor(private readonly broker: DataBroker<Project>, private readonly updateService: ProjectsUpdateService) {
-  }
+    constructor(private readonly broker: DataBroker<Project>, private readonly updateService: ProjectsUpdateService) {
+    }
 
-  projectUpdate(projectId: number): void {
-    this.updateService.triggerUpdate(projectId);
-  }
+    projectUpdate(projectId: number): void {
+        this.updateService.triggerUpdate(projectId).then(taskId => {
+            this.triggeredTaskId = taskId;
+        });
+    }
 
-  ngOnInit() {
-    this.broker.initialize(environment.pipwatchApiUrl, "projects/", Project);
-    this.updateService.initialize(environment.pipwatchApiUrl);
+    ngOnInit() {
+        this.broker.initialize(environment.pipwatchApiUrl, "projects/", Project);
+        this.updateService.initialize(environment.pipwatchApiUrl);
 
-    this.broker.getAll().then(response => {
-      this.listOfProjects = response;
-    }).catch(error => {});
-  }
+        this.broker.getAll().then(response => {
+            this.listOfProjects = response;
+        }).catch(error => {});
+    }
 }
