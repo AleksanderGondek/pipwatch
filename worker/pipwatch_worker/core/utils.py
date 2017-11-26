a61721c07bfe389f873b63aaa0536ef7af5e9701
AleksanderGondek <gondekaleksander@hotmail.com>
2017-11-26 13:24:32 +0100

    Worker - Added project flavour enum

--- worker/pipwatch_worker/core/utils.py
+++ worker/pipwatch_worker/core/utils.py
@@ -1,5 +1,13 @@
 """This module contains various helper functions used throughout the worker."""
+from enum import Enum
 import os
+
+
+class ProjectFlavour(Enum):
+    """Represents supported values for project flavor."""
+    GIT = "git"
+    GITHUB = "github"
+    GERRIT = "gerrit"
 
 
 def get_pip_script_name() -> str:
