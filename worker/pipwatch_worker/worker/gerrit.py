f3cbb1e95d0f7a037d7882398196510d98e51183
AleksanderGondek <gondekaleksander@hotmail.com>
2017-12-12 17:24:32 +0100

    Worker - Fix linting issue

--- worker/pipwatch_worker/worker/gerrit.py
+++ worker/pipwatch_worker/worker/gerrit.py
@@ -8,16 +8,11 @@ from pipwatch_worker.worker.commands import FromVirtualenv, RepositoriesCacheMix
 class GitReview(RepositoriesCacheMixin):  # pylint: disable=too-few-public-methods
     """Encapsulates logic of creating git-review and submitting it."""
 
-    def __init__(
-            self,
-            logger: Logger,
-            project_details: Project
-    ) -> None:
+    def __init__(self, logger: Logger, project_details: Project) -> None:
         """Create method instance."""
         super().__init__()
         self.log = logger or getLogger(__name__)
         self.project_details = project_details
-
         self.from_venv = FromVirtualenv(project_id=self.project_details.id)
 
     def __call__(self) -> None:
