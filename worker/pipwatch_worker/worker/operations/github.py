ef0fdd01a0553f51618d11d0821cf44c3de8962f
AleksanderGondek <gondekaleksander@hotmail.com>
2017-12-21 19:38:56 +0100

    Worker - Expanded initial implementation for github pr creation

--- worker/pipwatch_worker/worker/operations/github.py
+++ worker/pipwatch_worker/worker/operations/github.py
@@ -1,5 +1,8 @@
 """This module contains operations related to creating github pull request."""
 from logging import Logger
+from typing import Dict, Union
+
+import requests
 
 from pipwatch_worker.core.data_models import Project
 from pipwatch_worker.worker.operations.operation import Operation
@@ -8,6 +11,9 @@ from pipwatch_worker.worker.operations.operation import Operation
 class GitReview(Operation):  # pylint: disable=too-few-public-methods
     """Encapsulates logic of creating github pull-review."""
 
+    DEFAULT_PR_TITLE = "[Pipwatch] - Automatic increment of requirements versions."
+    DEFAULT_PR_BODY = ""
+
     def __init__(self, logger: Logger, project_details: Project) -> None:
         """Create method instance."""
         super().__init__(logger=logger, project_details=project_details)
@@ -15,3 +21,28 @@ class GitReview(Operation):  # pylint: disable=too-few-public-methods
     def __call__(self) -> None:
         """Create github pull request."""
         raise NotImplementedError()
+
+    def _get_pull_request_body(self) -> Dict[str, Union[bool, str]]:
+        """Prepare body for PR creation call."""
+        return {
+            "title": self.DEFAULT_PR_TITLE,
+            "head": "master",
+            "base": "master",
+            "body": self.DEFAULT_PR_BODY,
+            "maintainer_can_modify": True
+        }
+
+    def _create_pull_request(self) -> None:
+        """Call github http api to create a pull request."""
+        url = "{github_api_address}/repos/{owner}/{repo_name}/pulls".format(
+            github_api_address="TO_BE_LOADED_FROM_PROJECT",
+            owner="TO_BE_LOADED_FROM_PROJECT",
+            repo_name=self.project_details.name
+        )
+        payload = self._get_pull_request_body()
+        self.log.debug("About to perform POST request to address '{url}' with payload {payload}".format(
+            url=url,
+            payload=str(payload)
+        ))
+        response = requests.post(url=url, json=payload)
+        response.raise_for_status()
