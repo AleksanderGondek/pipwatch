8c2dda3b0208ab3dced55fb4e170b098f4124526
AleksanderGondek <gondekaleksander@hotmail.com>
2018-03-04 13:32:42 +0100

    Ui - Fixed links in top menu

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { AboutComponent } from "app/about/about.component";
 import { AppComponent } from "app/app.component";
+import { CreateProjectComponent } from "app/create-project/create-project.component";
 import { ProjectsOverviewComponent } from "app/projects-overview/projects-overview.component";
 import { ProjectDetailsComponent } from "app/project-details/project-details.component";
 
 const routes: Routes = [
     {path: "", redirectTo: "/home", pathMatch: "full"},
     {path: "home", component: ProjectsOverviewComponent},
+    {path: "new", component: CreateProjectComponent},
     {path: "about", component: AboutComponent},
     {path: "details/:id", component: ProjectDetailsComponent}
 ];
